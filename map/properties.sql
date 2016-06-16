939d3ba647c4a6aac003e07fff7d827d127392cb
Noah Coad <noah@coad.net>
2016-06-16 11:51:22 -0500

    before updating properties.json to just 2015

--- map/properties.sql
+++ map/properties.sql
@@ -1,4 +1,6 @@
 select offices.name as office_name, longitude, latitude, streetname, streetnum, city, zipcode 
 from properties 
 join offices on properties.office_id = offices.id 
-where stage_code = 'closed' and disp_close_date >= date_sub(now(), interval 1 year) 
+where stage_code = 'closed' and
+-- disp_close_date >= date_sub(now(), interval 1 year) 
+year(disp_close_date) = '2015'
